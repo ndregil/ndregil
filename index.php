7ef88ce8f1d0adc9f53eda28ab1a1714a9dbeaa1
ndregil <140181392+ndregil@users.noreply.github.com>
2023-07-21 21:33:55 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+$PageTitle="Ndregil Web";
+
+function customPageHeader(){?>
+  <!--Arbitrary HTML Tags-->
+<?php }
+
+include_once('system/header.php');
+
+//body contents go here
+include_once('system/menu.php');
+
+
+//footer
+include_once('system/footer.php');
+?>
